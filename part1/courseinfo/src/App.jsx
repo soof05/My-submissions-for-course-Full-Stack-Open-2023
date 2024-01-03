8a6aa7a560e35048fd26519c7745a2e45e177eeb
sel-ouaf <sel-ouaf@student.1337.ma>
2024-01-03 22:06:56 +0100

    Start the section c of part1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,63 +1,18 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.coursename}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part[0].name} exercise={props.part[0].exercises}/>
-      <Part part={props.part[1].name} exercise={props.part[1].exercises}/>
-      <Part part={props.part[2].name} exercise={props.part[2].exercises}/>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercise}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  const total = props.part[0].exercises + props.part[1].exercises + props.part[2].exercises;
-  return (
-    <p>
-      Number of exercises {total}
-    </p>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter);
 
   return (
-    <div>
-      <Header coursename={course.name}/>
-      <Content part={course.parts}/>
-      <Total part={course.parts}/>
-    </div>
+    <div>{counter}</div>
   )
 }
 
