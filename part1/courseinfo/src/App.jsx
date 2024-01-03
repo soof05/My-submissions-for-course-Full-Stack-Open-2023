62823343d2d38ed3e6241272f519b3f96f777a4a
sel-ouaf <sel-ouaf@student.1337.ma>
2024-01-03 20:52:06 +0100

    Solve course information step5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercise1}/>
-      <Part part={props.part2} exercise={props.exercise2}/>
-      <Part part={props.part3} exercise={props.exercise3}/>
+      <Part part={props.part[0].name} exercise={props.part[0].exercises}/>
+      <Part part={props.part[1].name} exercise={props.part[1].exercises}/>
+      <Part part={props.part[2].name} exercise={props.part[2].exercises}/>
     </div>
   )
 }
@@ -25,29 +25,38 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  const total = props.part[0].exercises + props.part[1].exercises + props.part[2].exercises;
   return (
     <p>
-      Number of exercises {props.total}
+      Number of exercises {total}
     </p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header coursename={course}/>
-      <Content part1={part1} exercise1={exercises1}
-        part2={part2} exercise2={exercises2}
-        part3={part3} exercise3={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header coursename={course.name}/>
+      <Content part={course.parts}/>
+      <Total part={course.parts}/>
     </div>
   )
 }
