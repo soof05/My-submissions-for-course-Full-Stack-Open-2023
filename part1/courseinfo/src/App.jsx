8c1dd58e55e72151a6dcd2ba5b0b1d957ce59503
sel-ouaf <sel-ouaf@student.1337.ma>
2024-01-05 21:04:59 +0100

    Learn The Basics Of React Hook (useState)

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from 'react'
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 const App = () => {
 
   const [ counter, setCounter ] = useState(0)
+  console.log('this will be displayed first')
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const increaseByOne = () => {
+    console.log('plus')
+    setCounter(counter + 1)
+  }
 
-  console.log('rendering...', counter);
+  const resetToZero = () => {
+    console.log('zero')
+    setCounter(0)
+  }
+
+  const decreaseByOne = () => {
+    console.log('minus')  
+    setCounter(counter - 1)
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text={'plus'}/>
+      <Button onClick={resetToZero} text={'reset'}/>
+      <Button onClick={decreaseByOne} text={'minus'}/>
+    </div>
   )
 }
 
