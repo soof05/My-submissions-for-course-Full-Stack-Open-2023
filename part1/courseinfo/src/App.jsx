d4f59ec6212eae09e12a4b3766e3da3b238d7e68
sel-ouaf <sel-ouaf@student.1337.ma>
2024-01-05 22:50:22 +0100

    Complex Example Of useState

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,52 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+const Display = ({counter}) => <>{counter}</>
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const History = (props) => {  
+  if (props.allClicks.length === 0) {
+    return (      
+      <div>        
+        the app is used by pressing the buttons      
+      </div>    
+      )  
+    }  
+    return (
+    <div>      
+      button press history: {props.allClicks.join(' ')}    
+      </div>  
+      )
+}
+
 const App = () => {
 
-  const [ counter, setCounter ] = useState(0)
-  console.log('this will be displayed first')
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => {
-    console.log('plus')
-    setCounter(counter + 1)
+  const handleLeftClick = () => {    
+    setAll(allClicks.concat('L'))
+    const update = left + 1;   
+    setLeft(update)  
+    setTotal(update + right)
   }
-
-  const resetToZero = () => {
-    console.log('zero')
-    setCounter(0)
-  }
-
-  const decreaseByOne = () => {
-    console.log('minus')  
-    setCounter(counter - 1)
+  const handleRightClick = () => {    
+    setAll(allClicks.concat('R')) 
+    const update = right + 1;   
+    setRight(update)
+    setTotal(update + left)  
   }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text={'plus'}/>
-      <Button onClick={resetToZero} text={'reset'}/>
-      <Button onClick={decreaseByOne} text={'minus'}/>
+      <Display counter={left}/>
+      <Button onClick={handleLeftClick} text={'left'}/>
+      <Button onClick={handleRightClick} text={'right'}/>
+      <Display counter={right}/>
+      <History allClicks={allClicks} />
+      <p>Total : {total}</p>
     </div>
   )
 }
