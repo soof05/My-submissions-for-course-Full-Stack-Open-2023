b33fbc289758904217027a2c1289d048ea84fb67
sel-ouaf <sel-ouaf@student.1337.ma>
2024-01-09 20:58:47 +0100

    Complete unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,12 +7,15 @@ const Button = ({clickHandler, feedback}) => {
 }
 
 const Statistics = (statistics) => {
+  // console.log(statistics)
   return (
     <p>
       good {statistics.good} <br/>
       neutral {statistics.neutral} <br/>
       bad {statistics.bad} <br/>
-      total {statistics.total}
+      total {statistics.total} <br/>
+      average {statistics.average} <br/>
+      positive {statistics.percentage}
     </p>
   )
 }
@@ -24,31 +27,61 @@ const App = () => {
   const [bad , setBad] = useState(0)
   const [total, setTotal] = useState(0)
   const [average, setAverage] = useState(0)
+  const [positivePercentage, setPositivePercentage ] = useState(0)
 
   const totalSetter = (value) => {
     setTotal(value)
   }
 
-  const AverageSetter = () => {
-    
+  const AverageSetter = (feedbacks) => {
+    const statsTotal = feedbacks.good + feedbacks.neutral + feedbacks.bad;
+    const updateAverage = (feedbacks.good * 1 + feedbacks.neutral * 0 + feedbacks.bad * (-1)) / statsTotal;
+    setAverage(updateAverage);
   }
 
   const goodSetter = () => {
     const update = good + 1
     setGood(update)
     totalSetter(update + neutral + bad)
+    const stats = {
+      good: update,
+      neutral: neutral,
+      bad: bad,
+    }
+    AverageSetter(stats);
+    positiveSetter(stats)
   }
 
   const neutralSetter = () => {
     const update = neutral + 1
     setNeutral(update)
     totalSetter(update + good + bad)
+    const stats = {
+      good: good,
+      neutral: update,
+      bad: bad,
+    }
+    AverageSetter(stats);
+    positiveSetter(stats)
   }
 
   const badSetter = () => {
     const update = bad + 1
     setBad(update)
     totalSetter(update + good + neutral)
+    const stats = {
+      good: good,
+      neutral: neutral,
+      bad: update,
+    }
+    AverageSetter(stats);
+    positiveSetter(stats)
+  }
+
+  const positiveSetter = (feedbacks) => {
+    let totalClicks = feedbacks.good + feedbacks.bad + feedbacks.neutral;
+    const percentage = feedbacks.good / totalClicks * 100;
+    setPositivePercentage(percentage);
   }
 
   return (
@@ -58,7 +91,7 @@ const App = () => {
       <Button clickHandler={neutralSetter} feedback={'neutral'}/>
       <Button clickHandler={badSetter} feedback={'bad'}/>
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} percentage={positivePercentage}/>
     </div>
   )
 }
